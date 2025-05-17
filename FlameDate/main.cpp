c5a1c773d78c82db2e75b3d317c40bde8a759bbc
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-05-17 14:17:57 +0300

    Final MVP

--- FlameDate/main.cpp
+++ FlameDate/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     FlameDate w;
+    w.setWindowIcon(QIcon("icon.png"));
+    w.setWindowTitle("FlameDate by Solovev");
     w.show();
     return a.exec();
 }
