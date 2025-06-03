a6e2f99be3f0b48eb164a983c7a7a71bc4a3aff1
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-06-03 12:37:58 +0300

    123

---
+++ source/GeneralParam.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <QMainWindow>
+#include "ui_GeneralParam.h"
+
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QString>
+
+class GeneralParam : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	GeneralParam(QWidget* parent = nullptr);
+	~GeneralParam();
+
+	void readDefaultConfig();
+	void fileNameSetter(QString any);
+	bool getNameCheck();
+	QList<int> getMessegeWeekMaskInGeneral();
+
+	QString m_name;
+	QString m_directory;
+	QString m_deadlineLine;
+	QString m_timeLine;
+	QString m_rowLine;
+	QString m_columnLine;
+	QString m_telegramLine;
+	QString m_list;
+	QString m_rowHead;
+
+	
+
+
+signals:
+	void refreshSetting();
+
+private slots:
+	void writeCurrent();
+
+private:
+	Ui::GeneralParamClass ui;
+	QString fileForSend;
+	QList<int>messegeWeekMaskInGeneral{ 0,0,0,0,0,0,0,0 };
+};
---
+++ source/TelegramJacket.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QtNetwork/QNetworkRequest>
+#include <QUrl>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QTimer>
+#include <QUrlQuery>
+#include <windows.h>
+#include <QFile.h>
+
+
+class TelegramJacket : public QObject
+{
+	Q_OBJECT
+
+public:
+	TelegramJacket(QObject* parent = nullptr);
+	~TelegramJacket();
+
+	void getUpdates();
+	void setToken(QString val);
+
+	void setWeekMask(QList<int> any);
+
+public slots:
+	//void sendMessage(const QString chatId, const QString message);
+
+	void sendMessage(const QString message, const QString d_message);
+
+public:
+	QString token = "";
+	qint64 iD;
+
+	QList<QString>idMassive;
+
+private:
+	QNetworkAccessManager* manager = nullptr;
+
+	QList<int>messegeWeekMask{ 0,0,0,0,0,0,0,0 };
+	bool startBool = false;
+};
+
+
