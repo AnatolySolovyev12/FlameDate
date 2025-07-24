f0c513bd331d97275c23b28f310066d49e0e2567
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-07-24 20:22:51 +0300

    123

--- FlameDate/main.cpp
+++ FlameDate/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    //SetConsoleOutputCP(1251);  // Установить UTF-8
+    SetConsoleOutputCP(1251);  // Установить UTF-8
 
     QCoreApplication a(argc, argv);
     FlameDate w;
